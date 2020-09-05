dc9e927860686d90395688db02ad034e4c0716a1
Meklit Cherie <makywonde347@yahoo.com>
2020-09-04 23:36:58 -0700

    Initial commit

---
+++ tutorial.py
@@ -0,0 +1,22 @@
+def instagram_data_usuage(hours,data_per_hour):
+    # input how many hours we have used on instagram 
+    # input how many data does instgram use per hour 
+    result = data_per_hour*hours
+    print(result)
+instagram_data_usuage(5,100)
+    # per hour 100MB
+
+# we need use colon everytime we start a function 
+# our function takes two inputs
+# we need to inputs to calculate how many total MB we spent in total
+# 
+
+
+
+
+
+
+
+
+
+
